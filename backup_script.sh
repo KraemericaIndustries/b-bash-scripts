bb638968b9e2e6f51aac5bfb8d0bf433ba982e58
KraemericaIndustries <bob.kraemer.git@gmail.com>
2024-09-28 21:40:11 -0400

    Updated backup_script

--- backup_script.sh
+++ backup_script.sh
@@ -2,7 +2,7 @@
 
 # Author: Bob Kraemer
 # Date Created: 09/26/2024
-# Last Modified: 09/26/2024
+# Last Modified: 09/28/2024
 
 # Description
 # Backs up all the files in my home directory (/home/bkraemer) and saves them in a tar archive
@@ -10,5 +10,14 @@
 # Usage
 # backup_script
 
-tar -cvf ~/scripts/my_backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar ~/* 2>/dev/null
+echo "Hello, ${USER^}"
+echo "I will now back up your home directory, $HOME"
+
+currentdir=$(pwd)
+
+echo "You are running this script from $currentdir."
+echo "Therefore, I will save the backup in $currentdir"
+
+tar -cf $currentdir/my_backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar ~/* 2>/dev/null
+echo "Backup Completed Successfully."
 exit 0
