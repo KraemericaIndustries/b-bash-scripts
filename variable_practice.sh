d8be79ac2efb6a5045aaa04c0b346fa7e402d35e
KraemericaIndustries <bob.kraemer.git@gmail.com>
2024-11-10 08:45:43 -0500

    Adding scripts upon course completion

---
+++ variable_practice.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Author: Bob Kraemer
+# Date Created: 09/28/2024
+# Last Modified: 09/28/2024
+
+# Description
+# Prints "This is my first script" to the terminal
+
+# Usage
+# our_first_script
+
+student="Sarah"
+echo "Hello ${student}"
+exit 0
